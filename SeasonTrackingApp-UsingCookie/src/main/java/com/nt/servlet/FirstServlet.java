8273052e8e14fd243f07562757849abca9fb15e3
iam-biswajeet <biswajitsahoo9955@gmail.com>
2022-09-26 11:14:51 +0530

    ja

---
+++ SeasonTrackingApp-UsingCookie/src/main/java/com/nt/servlet/FirstServlet.java
@@ -0,0 +1,69 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/firsturl")
+public class FirstServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		PrintWriter pw=resp.getWriter();
+		resp.setContentType("text/html");
+		//Read Form 1 Input
+		String pname=req.getParameter("pname");
+		String fname=req.getParameter("fname");
+		String paddrs=req.getParameter("paddrs");
+		String ms=req.getParameter("ms");
+		//Create InMemory Cookie
+		Cookie ck1=new Cookie("pname",pname);
+		Cookie ck2=new Cookie("fname",fname);
+		Cookie ck3=new Cookie("paddrs",paddrs);
+		Cookie ck4=new Cookie("ms",ms);
+		//Add cookeies
+		resp.addCookie(ck1);
+		resp.addCookie(ck2);
+		resp.addCookie(ck3);
+		resp.addCookie(ck4);
+		
+		
+		System.out.println(pname+"  "+fname+"  "+paddrs+"  "+ms);
+		pw.println("<h1 style='text-align:center;color:red'>Registration Form-2</h1>");
+		
+		pw.println("<form action='secondurl' method='POST'>");
+		pw.println("<table align='center' bgcolor='blue'>");
+		if(ms.equalsIgnoreCase("married")) {
+			pw.println("<tr><td>Spouse NAme::</td><td><input type='text' name='f2r1'></td></tr>");
+			
+				pw.println("<tr><td>No of child::</td><td><input type='text' name='f2r2'></td></tr>");
+		
+		}
+		else {
+			pw.println("<tr><td>Why are you want to marry::</td><td><input type='text' name='f2r1'></td></tr>");
+		
+				pw.println("<tr><td>When You MArry:</td><td><input type='text' name='f2r2'></td></tr>");
+							
+		}
+		
+		pw.println("<tr><td><input type='submit' value='register'></td><td><input type='reset' value='reset'></td></tr>");
+		pw.println("</table>");
+		
+		pw.println("<input type='hidden' name='hms' value='"+ms+"'>");
+		pw.println("</form>");
+		pw.close();
+			
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		doGet(req, resp);
+	}
+
+}
